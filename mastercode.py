7d7b9d4509193c283c68b4f2799079d1e65d2094
joehausman <joehausman@gmail.com>
2018-06-24 15:35:58 -0700

    cleanup, comment

--- mastercode.py
+++ mastercode.py
@@ -14,11 +14,11 @@ def generate_code():
         i += 1
     return rand
 
-# checks for guesses that are the correct color and in the correct spot
-# returns
+
+# checks for guesses that are the correct color and position
+# returns number of correct guesses
+# inserts placeholders to prevent future matches in white_check()
 def black_check(answer, guess):
-    # print('ans: ' + str(answer))
-    # print('gue: ' + str(guess))
     correct = 0
     i = 0
     while i < 4:
@@ -29,7 +29,8 @@ def black_check(answer, guess):
         i += 1
     return correct, answer, guess
 
-
+# checks for guesses that are correct in color only
+# assumes anything found by black_check() was replaced by placeholders
 def white_check(answer, guess):
     correct = 0
     i = 0
@@ -44,51 +45,37 @@ def white_check(answer, guess):
         i += 1
     return correct
 
+
 def print_row():
     CT = str(CURR_TURN)
-    # cb = str(correct_black)
-    # cw = str(correct_white)
-    # gu = ''.join(guess)
     print(CT + '|  |', end = '')
 
 def print_checks(correct_black, correct_white):
     cb = str(correct_black)
     cw = str(correct_white)
-    # print('_|' + cb + cw + '|____')
     print(' |' + cb + cw + '|    ')
     print('-|--|----')
-
-# def print_row_start():
-#     print(str(CURR_TURN) + '|', end = '')
-#
-# def print_row_end(correct_black, correct_white):
-#     cb = str(correct_black)
-#     cw = str(correct_white)
-#     print('|' + cb + cw)
 
 def print_code():
     global CODE
     print('\n     ' + ''.join(CODE) + ' <-- code\n')
 
 def win_game():
-    # @TODO: print win state thingy
     print_code()
     print('Well done!')
     exit(0)
 
 def lose_game():
     print_code()
-    # @TODO: print lose state thingy
-    # @TODO: print sad message
     print('Better luck next time :(')
     exit(0)
-#-----------------------------------------------------------
-# @TODO: max turn choice (via args)
+
 
 def setup():
+    # @TODO: max turn choice (via args)
+    # @TODO: instructions / help
     global CODE
     CODE = generate_code()
-    # print_code()    # DEBUGGING
     print('-|BW|----')
 
 def run():
@@ -97,14 +84,8 @@ def run():
     while CURR_TURN < MAX_TURNS:
         global CODE
         answer = CODE.copy()
-        # print_row_start()
         print_row()
-        # print_code()    # DEBUGGING
-        # @TODO: get player input
-        # guess = generate_code()    # DEBUGGING
-        # print(''.join(guess), end = '') # DEBUGGING
         guess = list(input())
-        # print('\b\b', end = '') # remove trailing newline from input
         guess_copy = guess.copy()
         correct_black, answer, guess_copy = black_check(answer, guess_copy)
         if correct_black == 4:
@@ -112,9 +93,6 @@ def run():
 
         correct_white = white_check(answer, guess_copy)
         print_checks(correct_black, correct_white)
-        # print_row(correct_black, correct_white, guess)
-        # print_row_end(correct_black, correct_white)
-
         CURR_TURN += 1
 
     lose_game()
