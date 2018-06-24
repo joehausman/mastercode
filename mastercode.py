7a83e38f9c995e1bb45f895c282925956f95d161
joehausman <joehausman@gmail.com>
2018-06-24 03:52:25 -0700

    made various changes; added virtually all functionality; need sleep

--- mastercode.py
+++ mastercode.py
@@ -1,16 +1,24 @@
 import random
 
 MAX_TURNS = 10
+CURR_TURN = 0
 CODECHARS = 'ABCDEF'
 
-CODE = ''
+CODE = []
 
 def generate_code():
-    return random.choice(CODECHARS)
+    rand = []
+    i = 0
+    while i < 4:
+        rand.append(random.choice(CODECHARS))
+        i += 1
+    return rand
 
 # checks for guesses that are the correct color and in the correct spot
 # returns
 def black_check(answer, guess):
+    # print('ans: ' + str(answer))
+    # print('gue: ' + str(guess))
     correct = 0
     i = 0
     while i < 4:
@@ -36,15 +44,58 @@ def white_check(answer, guess):
         i += 1
     return correct
 
+def print_row(correct_black, correct_white, guess):
+    CT = str(CURR_TURN)
+    cb = str(correct_black)
+    cw = str(correct_white)
+    gu = ''.join(guess)
+    print(CT + '|' + cb + cw + '|' + gu)
+
+def print_code():
+    global CODE
+    print('code:' + ''.join(CODE))
+
+def win_game():
+    # @TODO: print win state thingy
+    print_code()
+    print('Well done!')
+    exit(0)
+
+def lose_game():
+    print_code()
+    # @TODO: print lose state thingy
+    # @TODO: print sad message
+    print('Better luck next time :(')
+    exit(0)
 #-----------------------------------------------------------
 # @TODO: max turn choice (via args)
 
-# CODE = generate_code()
-#
-#
-#
-# correct_black, answer, guess = black_check(answer, guess)
-# if correct_black == 4:
-#     win_game()
-#
-# correct_white = white_check(answer, guess)
+def setup():
+    global CODE
+    CODE = generate_code()
+    # print_code()    # DEBUGGING
+    print('-|BW|----')
+
+def run():
+    global CURR_TURN
+    global MAX_TURNS
+    while CURR_TURN < MAX_TURNS:
+        global CODE
+        answer = CODE.copy()
+        # print_code()    # DEBUGGING
+        # @TODO: get player input
+        guess = generate_code()    # DEBUGGING
+        guess_copy = guess.copy()
+        correct_black, answer, guess_copy = black_check(answer, guess_copy)
+        if correct_black == 4:
+            win_game()
+
+        correct_white = white_check(answer, guess_copy)
+        print_row(correct_black, correct_white, guess)
+
+        CURR_TURN += 1
+
+    lose_game()
+
+setup()
+run()
